a5aeae2d170d0759d0f2e587e7ebc8e4496f62a4
support-citrupoker <support@citrupoker.com>
2026-03-05 14:14:14 +0000

    save

--- services/ghl.service.js
+++ services/ghl.service.js
@@ -531,7 +531,7 @@ async getAllConversationMessages(conversationId, locationId = this.locationId, m
       const params = {
         conversationId: conversationId,
         limit: pageSize,
-        type: "TYPE_INTERNAL_COMMENTS"
+        type: "TYPE_SMS"
       };
 
       // Add lastMessageId for pagination if we have it
